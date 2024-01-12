0be5506631f87eddae0aa4256822d8143d93162c
Ranti Maharani <rantimaharani1011@gmail.com>
2024-01-12 07:21:25 +0700

    menambahkan depan.php

---
+++ depan.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>myPerpus</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="container" style:"margin-top: 64px;">
+            <h1 align="center">Selamat Datang di myPerpus</h1>
+        </div>
+    </body>
+</html>
